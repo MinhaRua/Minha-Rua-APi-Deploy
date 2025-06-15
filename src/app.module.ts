9e871107f3fd25be34ac4a178111cfdb6668c1ff
GabrielTDev10 <gabrielgtodev@gmail.com>
2025-06-15 01:19:06 -0300

    first commit1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsuarioModule } from './usuario/usuario.module';
+import { UsuarioModule } from './Usuario/usuario.module';
 import { AuthModule } from './auth/auth.module';
 import { LocationsModule } from './locations/locations.module';
 
