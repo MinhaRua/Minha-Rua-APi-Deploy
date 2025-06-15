e99c7922593cdd2694cb916e07b39c4db3ac6144
GabrielTDev10 <gabrielgtodev@gmail.com>
2025-06-15 01:43:10 -0300

    first commit2

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -2,6 +2,9 @@ import { DataSource } from 'typeorm';
 import { Usuario } from 'src/Usuario/usuario.entity';
 import { Locations } from 'src/locations/locations.entity';
 import { Token } from 'src/token/token.entity';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const databaseProviders = [
   {
@@ -9,12 +12,12 @@ export const databaseProviders = [
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        username: 'root',
-        password: '',
-        database: 'Minharua',
-        entities: [Usuario, Locations,Token], 
+        host: process.env.MYSQLHOST,
+        port: parseInt(process.env.MYSQLPORT || '3306'),
+        username: process.env.MYSQLUSER,
+        password: process.env.MYSQLPASSWORD,
+        database: process.env.MYSQLDATABASE,
+        entities: [Usuario, Locations, Token],
         synchronize: false,
       });
 
