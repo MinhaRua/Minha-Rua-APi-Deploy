e3500d75d4ed093bbdeff673782c99652e3700aa
GabrielTDev10 <gabrielgtodev@gmail.com>
2025-06-17 08:10:38 -0300

    first commit17

--- src/locations/dto/locations.cadastrar.dto.ts
+++ src/locations/dto/locations.cadastrar.dto.ts
@@ -3,7 +3,6 @@ export interface locationsCadastrarDto{
     descricao: string,
     latitude: string,
     longitude: string,
-    created_at: Date,
     foto_local: string;
     endereco: string;
 }
